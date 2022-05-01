a5a92c97a8d032cff78158898acd6a722567c360
dougassis <dougassis@icloud.com>
2022-05-01 20:18:13 +0100

    CRUD Scaffolding

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC
 {
@@ -25,6 +27,9 @@ namespace SalesWebMVC
         {
             services.AddControllersWithViews();
             services.AddMvc();
+
+            services.AddDbContext<SalesWebMVCContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("SalesWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
